1577489e1328ca16d2ff29cebc3bb7bd976398d6
juanes G <jgalvez98@gmail.com>
2023-09-18 19:44:19 -0400

    confg files added, modules and routes code added/fixed

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -2,19 +2,33 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
 
-class ProductTag extends Model {}
+class ProductTag extends Model { }
 
 ProductTag.init(
-  {
-    // define columns
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product_tag',
-  }
+	{
+		// define columns
+		id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true
+		},
+		product_id: {
+			type: DataTypes.INTEGER,
+			references: {
+				model: 'product',
+				key: 'id',
+				unique: false
+			},
+		}
+	},
+	{
+		sequelize,
+		timestamps: false,
+		freezeTableName: true,
+		underscored: true,
+		modelName: 'product_tag',
+	}
 );
 
 module.exports = ProductTag;
